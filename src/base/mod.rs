6fed3cf9fd0ec4a8402c0067769c37fd7e3051f7
Chris Patuzzo <chris@patuzzo.co.uk>
2018-03-16 12:55:56 +0000

    Use slices instead of vectors to make the interface more flexible

--- src/base/mod.rs
+++ src/base/mod.rs
@@ -12,21 +12,21 @@ pub struct SlidingPuzzle {
 }
 
 impl SlidingPuzzle {
-    pub fn new(vec_2d: &Vec<Vec<u8>>) -> Result<Self> {
-        Self::must_be_rectangular(vec_2d)?;
-        Self::must_not_be_empty(vec_2d)?;
+    pub fn new(slice_2d: &[&[u8]]) -> Result<Self> {
+        Self::must_be_rectangular(slice_2d)?;
+        Self::must_not_be_empty(slice_2d)?;
 
-        let rows = Self::number_of_rows(vec_2d);
-        let columns = Self::number_of_columns(vec_2d);
-        let tiles = Self::flatten(vec_2d);
+        let rows = Self::number_of_rows(slice_2d);
+        let columns = Self::number_of_columns(slice_2d);
+        let tiles = Self::flatten(slice_2d);
 
         Self::must_contain_one_blank(&tiles)?;
 
         Ok(Self { tiles, rows, columns })
     }
 
-    fn must_be_rectangular<T>(vec_2d: &Vec<Vec<T>>) -> Result<()> {
-        let lengths = vec_2d.iter().map(|row| row.len());
+    fn must_be_rectangular<T>(slice_2d: &[&[T]]) -> Result<()> {
+        let lengths = slice_2d.iter().map(|row| row.len());
         let uniques = HashSet::<usize>::from_iter(lengths);
 
         if uniques.len() > 1 {
@@ -36,9 +36,9 @@ impl SlidingPuzzle {
         }
     }
 
-    fn must_not_be_empty<T>(vec_2d: &Vec<Vec<T>>) -> Result<()> {
-        let no_rows = vec_2d.len() == 0;
-        let no_columns = vec_2d.iter().any(|row| row.len() == 0);
+    fn must_not_be_empty<T>(slice_2d: &[&[T]]) -> Result<()> {
+        let no_rows = slice_2d.len() == 0;
+        let no_columns = slice_2d.iter().any(|row| row.len() == 0);
 
         if no_rows || no_columns {
             Err(SlidingPuzzleError::new("puzzle must not be empty"))
@@ -47,8 +47,8 @@ impl SlidingPuzzle {
         }
     }
 
-    fn must_contain_one_blank(vec: &Vec<u8>) -> Result<()> {
-        let blanks = vec.iter().filter(|&&e| e == 0).count();
+    fn must_contain_one_blank(slice: &[u8]) -> Result<()> {
+        let blanks = slice.iter().filter(|&&e| e == 0).count();
 
         if blanks != 1 {
             Err(SlidingPuzzleError::new("puzzle must contain a single blank"))
@@ -57,16 +57,16 @@ impl SlidingPuzzle {
         }
     }
 
-    fn number_of_rows<T>(vec_2d: &Vec<Vec<T>>) -> usize {
-        vec_2d.len()
+    fn number_of_rows<T>(slice_2d: &[&[T]]) -> usize {
+        slice_2d.len()
     }
 
-    fn number_of_columns<T>(vec_2d: &Vec<Vec<T>>) -> usize {
-        vec_2d.first().unwrap().len()
+    fn number_of_columns<T>(slice_2d: &[&[T]]) -> usize {
+        slice_2d.first().unwrap().len()
     }
 
-    fn flatten<T: Copy>(vec_2d: &Vec<Vec<T>>) -> Vec<T> {
-        vec_2d.iter().flat_map(|row| row.iter().cloned()).collect()
+    fn flatten<T: Copy>(slice_2d: &[&[T]]) -> Vec<T> {
+        slice_2d.iter().flat_map(|row| row.iter().cloned()).collect()
     }
 }
 
