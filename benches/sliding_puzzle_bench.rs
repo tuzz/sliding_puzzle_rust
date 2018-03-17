911c89497759665995722fa84d017a6222853cd1
Chris Patuzzo <chris@patuzzo.co.uk>
2018-03-17 17:32:41 +0000

    Benchmark #new

--- benches/sliding_puzzle_bench.rs
+++ benches/sliding_puzzle_bench.rs
@@ -4,15 +4,8 @@ extern crate sliding_puzzle;
 use bencher::Bencher;
 use sliding_puzzle::SlidingPuzzle;
 
-fn benchmark_hello(bench: &mut Bencher) {
-    bench.iter(|| {
-        SlidingPuzzle::hello();
-    })
-}
+mod new;
 
-benchmark_group!(
-    benches,
-    benchmark_hello
+benchmark_main!(
+    new::benches,
 );
-
-benchmark_main!(benches);
