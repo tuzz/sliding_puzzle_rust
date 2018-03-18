16b074592b74562af8e5c8056e2b594b859d5217
Chris Patuzzo <chris@patuzzo.co.uk>
2018-03-18 23:54:18 +0000

    Improve search time by ~15% by not doubling back

--- src/bin/search.rs
+++ src/bin/search.rs
@@ -19,8 +19,8 @@ fn main() {
     println!("estimated compute time: {:.2} minutes\n", time);
 
     let mut seen = BitSet::with_capacity(max as usize);
-    let mut queue = VecDeque::<u64>::new();
-    let mut next_queue = VecDeque::<u64>::new();
+    let mut queue = VecDeque::<(u64, Direction)>::new();
+    let mut next_queue = VecDeque::<(u64, Direction)>::new();
     let mut depth = -1;
     let mut depth_of_max = -1;
     let mut width = 0;
@@ -28,31 +28,35 @@ fn main() {
     let mut percent = 0.0;
     let mut hardest = 0;
 
-    queue.push_back(0);
+    queue.push_back((0, Direction::Left));
 
     while !queue.is_empty() {
         depth += 1;
         width = queue.len();
         percent += queue.len() as f64 / (max as f64 / 2.0) * 100.0;
-        hardest = queue.front().unwrap().clone();
+        hardest = queue.front().unwrap().0.clone();
 
         println!("{:8.1}%  |  depth: {}, width: {}", percent, depth, width);
 
-        while let Some(decimal) = queue.pop_back() {
+        while let Some((decimal, previous)) = queue.pop_back() {
             let puzzle = SlidingPuzzle::from_decimal_unchecked(decimal, rows, columns);
 
             for direction in puzzle.moves() {
+                if direction == previous.opposite() {
+                    continue;
+                }
+
                 let next_decimal = puzzle.slide_unchecked(&direction)
                                          .to_decimal_unchecked();
 
                 if seen.insert(next_decimal as usize) {
-                    next_queue.push_back(next_decimal);
+                    next_queue.push_back((next_decimal, direction));
                 }
             }
         }
 
         queue = next_queue;
-        next_queue = VecDeque::<u64>::with_capacity(queue.len());
+        next_queue = VecDeque::<(u64, Direction)>::with_capacity(queue.len());
 
         if width > max_width {
             max_width = width;
@@ -106,12 +110,12 @@ fn estimate_memory(rows: usize, columns: usize, max: u64) -> f64 {
         _ => 0,
     };
 
-    estimate += max_width * 8; // queue
+    estimate += max_width * 9; // queue
     estimate as f64 / 8.0 / 1024.0 / 1024.0 / 1024.0 // GB
 }
 
 fn estimate_time(max: u64) -> f64 {
-    let rate_per_second = 1_055_000;
+    let rate_per_second = 1_241_000;
     let states = max / 2;
 
     states as f64 / rate_per_second as f64 / 60.0
